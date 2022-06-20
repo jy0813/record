f45e54da9344491118c003841131f71d2d40f190
jy0813 <75197188+jy0813@users.noreply.github.com>
2022-06-20 22:22:46 +0900

    feat: 변수 이름 짓기

---
+++ master/2.naming.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//Nameing Variables - 저장된 값을 잘 나타낼 수 있는 의미있는 이름 (예약어는 사용할 수 없음.)
+
+/** 변수규칙
+ *  라틴문자 (0-9, a-z, A-Z), _
+ *  대소문자를 구분함
+ *  camelCase 로 작성
+ *  예약어 ❌
+ *  숫자로 시작 ❌
+ *  특수문자 ❌ (_, $ 두가지는 예외)
+ *  여러개의 변수를 1, 2 ,3 숫자로 구분 ❌ -> 최대한 의미있게 구체적으로,
+ */
+
+let apple;
+let redApple;
+
+//나쁜예제
+
+let num = 20;
+let audio1;
+let audio2;
+
+//좋은예제
+
+let myAge = 20;
+let audioBackground;
+let audioWind;
