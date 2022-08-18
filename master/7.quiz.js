3d674a2f12f2ba9bb403e7241a0c357158d8fc49
jy0813 <busbug3@gmail.com>
2022-08-18 21:03:52 +0900

    feat: 구조분해 quiz

---
+++ master/7.quiz.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const prop = {
+  name: "Button",
+  styles: {
+    size: 20,
+    color: "black",
+  },
+};
+
+function changeColor({ styles: { color } }) {
+  console.log(color);
+}
+
+changeColor(prop);
